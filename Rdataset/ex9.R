36240ae2c63716b1c413b1e11fabd1224a7fc5d1
gdwangh <gdwangh@189.cn>
2015-02-04 22:56:40 +0800

    update ex for day 9

--- Rdataset/ex9.R
+++ Rdataset/ex9.R
@@ -1,6 +1,7 @@
 install.packages("MASS")
 
-setwd("D:/workspace/The Caltech-JPL Summer School on Big Data Analytics/coursera-JPL-bigdata/Rdataset")
+# setwd("D:/workspace/The Caltech-JPL Summer School on Big Data Analytics/coursera-JPL-bigdata/Rdataset")
+setwd("D:/doc/study/JPL summer school big data/coursera-JPL-bigdata/Rdataset")
 cid<-read.table("Thompson_data/Citrus_Identification_Data.txt",sep=",",header = TRUE)
 ciw<-read.table("Thompson_data/Citrus_Identification_Wavelengths.txt",sep=",")
 cvd<-read.table("Thompson_data/Citrus_Varieties_Data.txt",sep=",",header = TRUE)
@@ -21,6 +22,11 @@ plot(wavelength1, wavelength2)
 
 # Perform a Principal Component Analysis (PCA).
 X=cid[,c(4:429)]
+
+library(caret)
 nsv<-nearZeroVar(X)
-cidPC<-preProcess(X[,-nsv],method="pca", pcaComp=2)
+cidPC<-preProcess(X[,-nsv],method="pca")
 head(cidPC$rotation)
+V<- cidPC$rotation
+plot(V[,1], V[,3])
+
